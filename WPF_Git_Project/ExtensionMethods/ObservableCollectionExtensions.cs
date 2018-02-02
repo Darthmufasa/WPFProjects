248afa6e44bf2239a0a0ff99bd5fadb3600560cd
Luke Bailey <Darthmufasa@live.com>
2018-02-02 14:38:47 -0500

    Added Extension Methods Project. Added ObservableCollectionExtensions.cs

---
+++ WPF_Git_Project/ExtensionMethods/ObservableCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class ObservableCollectionExtensions
+    {
+        public static void AddRange<T>(this ObservableCollection<T> collection, IEnumerable<T> collectionToAdd)
+        {
+            if (collectionToAdd != null)
+            {
+                foreach (var i in collectionToAdd)
+                {
+                    collection.Add(i);
+                }
+            }
+        }
+    }
+}
