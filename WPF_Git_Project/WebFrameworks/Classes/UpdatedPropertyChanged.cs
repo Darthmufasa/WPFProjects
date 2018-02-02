03a5e1548f1ac25fc992318bdd67a7e0265a472d
Luke Bailey <Darthmufasa@live.com>
2018-02-02 12:30:44 -0500

    Added Interface and Class Implementation for UpdatedPropertyChanged

---
+++ WPF_Git_Project/WebFrameworks/Classes/UpdatedPropertyChanged.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WebFrameworks.Interfaces;
+
+namespace WebFrameworks.Classes
+{
+    public class UpdatedPropertyChanged : IUpdatedPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void SetProperty<T>(ref T refField, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if(!Equals(refField, newValue))
+            {
+                refField = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ WPF_Git_Project/WebFrameworks/Interfaces/IUpdatedPropertyChanged.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebFrameworks.Interfaces
+{
+    interface IUpdatedPropertyChanged : INotifyPropertyChanged
+    {
+        new event PropertyChangedEventHandler PropertyChanged;
+        void SetProperty<T>(ref T refField, T newValue, string propertyName);
+    }
+}
